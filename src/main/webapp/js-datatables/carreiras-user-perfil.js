ea01a4702b78fe8b46d9cd4145ac97ac2cc7bc12
grenne <grenneglr@gmail.com>
2016-12-01 14:44:34 -0200

    concluida as alteracoes do Eric

--- src/main/webapp/js-datatables/carreiras-user-perfil.js
+++ src/main/webapp/js-datatables/carreiras-user-perfil.js
@@ -64,22 +64,44 @@
         		(append == "carreiras_user_perfil_theader" && percentualHabilidades != 100)	){
             	var habilidades = "";
             	$.each(carreira.arrayNecessarios, function (z, habilidade){
+            		var disabled = "";
+            		var textoTip = "quero aprender";
+            		if (checaUserPerfilElemento("habilidadeInteresse", habilidade.idHabilidade)){
+            			disabled = 'disabled';
+            			textoTip = "marquei interesse";	
+            		};
+            		var disabled_2 = "";
+            		var textoTip_2 = "sei fazer";
+            		if (checaUserPerfilElemento("habilidades", habilidade.idHabilidade)){
+            			disabled_2 = 'disabled';
+            			textoTip_2 = "ja possuo";	
+            		};
             		habilidades = habilidades +
        				'<div class="row">' +
 	       				'<div class="user-panel-habilidade-div col-xs-12">' +
 		    				'<span class="user-panel-carreira-habilidade-nome habilidadeCarreira col-xs-9">- ' + habilidade.name + '</span>' +
-							'<button id="cursoHabilidadeIn_' + i + "-" + z + '" class="user-panel-button-curso user-panel-button-curso-item col-xs-2"><span><i class="fa fa-chevron-down "></i> cursos</span></button>' +
-							'<button id="cursoHabilidadeOff_' + i + "-" + z + '" class="user-panel-button-curso col-xs-2 hide"><span><i class="fa fa-chevron-up "></i> cursos</span></button>' +
+		    				'<span><a id="seiFazerHabilidade_' + i + "-" + z + '" data-tooltip="' + textoTip_2 + '" data-toggle="modal" data-idHabilidade="' + habilidade.idHabilidade + '"  class="' + disabled_2 + ' seiFazerHabilidade"><i class="fa fa-book"></i></a></span>' +
+		    				'<span><a id="queroAprenderHabilidade_' + i + "-" + z + '" data-tooltip="' + textoTip + '" data-idHabilidade="' + habilidade.idHabilidade + '" class="' + disabled + ' queroAprenderHabilidade"><i class="fa fa-leanpub"></i></a></span>' +
+							'<button id="cursoHabilidadeIn_' + i + "-" + z + '" class="user-panel-button-curso user-panel-button-curso-item col-xs-2 cursoHabilidadeIn"><span><i class="fa fa-chevron-down "></i> cursos</span></button>' +
+							'<button id="cursoHabilidadeOff_' + i + "-" + z + '" class="user-panel-button-curso col-xs-2 hide cursoHabilidadeOff"><span><i class="fa fa-chevron-up "></i> cursos</span></button>' +
 						'</div>' +
-					'</div>';
-                	$.each(habilidade.cursos, function (w, curso){
-                		habilidades = habilidades +
-           				'<div class="row">' +
-           					'<div class="user-panel-curso-div col-xs-12">' +
-           						'<span class="user-panel-curso-habilidade-nome cursoHabilidade_' + i + "-" + z + ' col-xs-12 hide">- <a  href="' + curso.documento.wiki + '" target="_blank">  ' + curso.documento.descricao + '</a></span>' +
-    						'</div>' +
-           				'</div>';
-                	});
+					'</div>';                	
+             	$.each(habilidade.cursos, function (w, curso){
+            		var disabled = "";
+            		var textoTip = "quero fazer";
+            		if (checaUserPerfilElemento("cursoInteresse", curso.documento.idCurso)){
+            			disabled = 'disabled';
+            			textoTip = "marquei interesse";	
+            		};
+            		habilidades = habilidades +
+       				'<div class="row">' +
+       					'<div class="user-panel-curso-div col-xs-12">' +
+       						'<span class="user-panel-curso-habilidade-nome cursoHabilidade_' + i + "-" + z + ' col-xs-12 hide">- <a  href="' + curso.documento.wiki + '" target="_blank">  ' + curso.documento.descricao + '</a>' +
+       						'&nbsp;&nbsp;&nbsp;<a id="querofazercurso_' + i + "-" + z + "-" + w + '" data-tooltip="' + textoTip + '"  class="hide querofazercurso icon-querofazer ' + disabled + ' cursoHabilidade_' + i + "-" + z + '"  data-idCurso="' + curso.documento.idCurso + '"><i class="fa fa-leanpub icon-quefazer"></i></a>' +
+       						'</span>' +
+						'</div>' +
+       				'</div>';
+            	});
             	});
 	        	actions = "";
 	        	if (append == "carreiras_user_perfil_theader"){
@@ -102,9 +124,59 @@
 							'</div>' +
 						'</td>' +
 					'</tr>';
+	        	console.log ("**********************************************************************");
+	        	console.log ("**********************************************************************");
+	        	console.log ("**********************************************************************");
+	        	console.log (habilidades);
 	        	$("#carreira_user_perfil_tbody" + append).append(carreira_user_perfil_table_row);
-	        	$('#acaoCarreiraUserPerfilHabilidades' + i + append).off('click');
+
+	        	$('.queroAprenderHabilidade').off('click');
+        	    $('.queroAprenderHabilidade').on('click',function(){
+        			$("#" + $(this).attr('id')).addClass("disabled");
+        	    	var objJson = JSON.parse(localStorage.getItem("meuPerfil"));
+        	    	atualizaUserPerfilElemento (objJson, "habilidadeInteresse", $(this).attr('data-idhabilidade'));
+        	    	atualizaMapa ($(this).attr('data-idhabilidade'), "have", "2");
+        	    });
+
+        	    $('.seiFazerHabilidade').off('click');
+        	    $('.seiFazerHabilidade').on('click',function(){
+        			$("#" + $(this).attr('id')).addClass("disabled");
+        	    	var objJson = JSON.parse(localStorage.getItem("meuPerfil"));
+        	    	atualizaUserPerfilElemento (objJson, "habilidade", $(this).attr('data-idhabilidade'));
+        	    	atualizaMapa ($(this).attr('data-idhabilidade'), "have", "0");
+        	    });
+
+        	    $('.querofazercurso').each(function( z ) {
+            		$("#" + $(this).attr('id')).off('click');
+            		$("#" + $(this).attr('id')).on('click',function(){
+            			$("#" + $(this).attr('id')).addClass("disabled");
+            	    	var objJson = JSON.parse(localStorage.getItem("meuPerfil"));
+            	    	atualizaUserPerfilElemento (objJson, "cursoInteresse", $(this).attr('data-idcurso'));
+            	    });
+            	});
+
+        	    $('.cursoHabilidadeIn').off('click');
+        	    $('.cursoHabilidadeIn').on('click', function(){
+        			var id = $(this).attr('id');
+        			var i = id.split("_")[1];
+    				$( ".cursoHabilidade_" + i).removeClass('hide');
+    				$( "#cursoHabilidadeOff_" + i).removeClass('hide');
+    				$( "#cursoHabilidadeIn_" + i).addClass('hide');
+    			});
+
+        	    $('.cursoHabilidadeOff').off('click');
+        	    $('.cursoHabilidadeOff').on('click', function(){
+        			var id = $(this).attr('id');
+        			var i = id.split("_")[1];
+    				$( ".cursoHabilidade_" + i).addClass('hide');
+    				$( "#cursoHabilidadeOff_" + i).addClass('hide');
+    				$( "#cursoHabilidadeIn_" + i).removeClass('hide');
+    			});
+
+    			$('#acaoCarreiraUserPerfilHabilidades' + i + append).off('click');
 	            $('#acaoCarreiraUserPerfilHabilidades' + i + append).on('click', function () {
+	    			var id = $(this).attr('id');
+	    			var i = id.split("_")[1];
 	        		$('.paineis-user-perfil').addClass("hide");
 	        		$('.habilidade-user-perfil').removeClass("hide");
 					rest_obterUserPerfilItens("habilidades-interesse-carreiras", carregaHabilidadesUserPerfil, semAcao, "habilidades-necessarias-carreira", carreira.nome, "habilidades_user_perfil_theader");
@@ -120,21 +192,6 @@
 
         var carreira_user_perfil = $('#carreira_user_perfil_list' + append);
         
-		$(".user-panel-button-curso-item").each(function() {
-			var id = $(this).attr('id');
-			var i = id.split("_")[1];
-			$('#cursoHabilidadeIn_' + i).on('click', function(){
-				$( ".cursoHabilidade_" + i).removeClass('hide');
-				$( "#cursoHabilidadeOff_" + i).removeClass('hide');
-				$( "#cursoHabilidadeIn_" + i).addClass('hide');
-			});
-			$('#cursoHabilidadeOff_' + i).on('click', function(){
-				$( ".cursoHabilidade_" + i).addClass('hide');
-				$( "#cursoHabilidadeOff_" + i).addClass('hide');
-				$( "#cursoHabilidadeIn_" + i).removeClass('hide');
-			});
-		});
-
         carreira_user_perfil.footable().trigger('footable_collapse_all');
 
 		$('#collapseCarreirasUserPerfilConquista').on('click', function(){
